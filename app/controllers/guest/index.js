f982bf386a2daa5a89d6df8c2237feeffbf56bc3
1612465 <1612465@student.hcmus.edu.vn>
2020-01-03 18:17:00 +0700

    Sua doi Category o Trang chu

--- app/controllers/guest/index.js
+++ app/controllers/guest/index.js
@@ -1,15 +1,67 @@
 const { Product } = require('../../models/product')
 const { Category } = require('../../models/category')
 const mongoose = require('mongoose');
+
+const moment = require('moment')
+        moment.locale('vi-VN');
+
 module.exports = {
     getHome: async function(req, res) {
+        
+        var currentdate = new Date;
         const category = await Category.find({});
-        const product = await Product.find({},(err,doc)=>{}).limit(5);
+        const ProductNearEnd = await Product.find(
+            {timeEnd:{$gt:currentdate}},
+            (err,doc)=>{}
+        )
+        .populate({
+            path: 'bidders.bidder',
+            populate: {
+                path: 'user'
+            }
+        })
+        .sort({timeEnd:-1})
+        .limit(5);
 
+        const MostExpensiveProduct= await Product.find(
+            {timeEnd:{$gt:currentdate}},
+            (err,doc)=>{}
+        )
+        .populate({
+            path: 'bidders.bidder',
+            populate: {
+                path: 'user'
+            }
+        })
+        .sort({priceCurrent:-1})
+        .limit(5);
+
+        const HottestProduct= await Product.find(
+            {timeEnd:{$gt:currentdate}},
+            (err,doc)=>{}
+        )
+        .populate({
+            path: 'bidders.bidder',
+            populate: {
+                path: 'user'
+            }
+        })
+        .sort({bidders:-1})
+        .limit(5);
         
         res.render('pages/guest/index',{
             category,
-            product
+            ProductNearEnd,
+            MostExpensiveProduct,
+            HottestProduct,
+            isAuthenticated : req.user,
+            moment
         });
     },
+    logOut: (req, res) => {
+        console.log('voday')
+        req.logOut();
+        res.redirect('/')
+    }
 }
+
