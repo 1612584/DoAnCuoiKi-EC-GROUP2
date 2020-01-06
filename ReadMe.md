906c14ceb27839f307ca75bbf25aeb30ec530df8
1612570 <1612570@student.hcmus.edu.vn>
2020-01-07 06:28:32 +0700

    Cap nhat product form va sidebar

--- ReadMe.md
+++ ReadMe.md
@@ -6,3 +6,5 @@
 +. npm install - express
 +. npm install --save express-handlebars sequelize : để tạo node_modules 
 +. Tùy theo máy mà config db Dev khác nhau => nên thêm dòng vào .gitignore để không ignore file config evironment database,host lên git. Ví dụ nên ignore db fileDir = configs/config.js => thêm vào .gitignore dòng configs/config.js
+
+DB_URL=mongodb+srv://test:mongodb@ec-4rl3d.mongodb.net/test?retryWrites=true&w=majority
--- app/routes/seller/index.js
+++ app/routes/seller/index.js
@@ -18,6 +18,82 @@ router.get('/', auth, seller, sellerController.get);
 router.post('/products/post', auth, seller, cpUpload, sellerController.postProduct)
 router.get('/products/getList', auth, seller,sellerController.getProductList);
 router.get('/products/getList/filter', auth, seller, sellerController.getProductListAndFilter);
-// router.get('/api/check_for_bid/:idProduct/:idBidder', sellerController.checkForBid)
 
+router.get('/products/:ID', auth, seller, async function (req, res) {
+    var productID = req.params.ID;
+    var productdetail = await Product.findOne({
+        _id: productID
+    }).populate('seller')
+        .populate({
+            path: 'bidders.bidder',
+            populate: {
+                path: 'user'
+            }
+        })
+    res.render('pages/seller/productDetail', {
+        product: productdetail
+    })
+})
+
+router.get('/:ID1/ban/:ID2', auth, seller, async function (req, res) {
+    var productID = req.params.ID1;
+    var bidderBanID = req.params.ID2;
+    var product = await Product.findById({ _id: productID }).exec((err, result) => {
+        if (result) {
+            for (var i = 0; i < result.bidders.length; i++) {
+                if (bidderBanID == result.bidders[i].bidder) {
+                    result.bidders[i].isWaitingSeller = true;
+                    result.save();
+                }
+            }
+        }
+    }); 
+    await sleep(5000);
+    res.redirect(`/sellers/products/${productID}`);
+})
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+
+router.post('/products/:ID', auth, seller, async function (req, res) {
+    var productID = req.params.ID;
+    var productdetail = await Product.find({
+        _id: productID
+    });
+    var testvalue = req.body.description;
+    var date = new Date;
+    today = formatDate(date);
+    var Newdescription = productdetail[0].fullDes + '<br>' + today + '</br>' + testvalue;
+    Product.findById({ _id: productID }, (err, product) => {
+        if (err) res.json(err)
+        else {
+            if (product == null || product == undefined) {
+                res.json('not found')
+            } else {
+                product.fullDes = product.fullDes + today + testvalue;
+                product.save({}, (err, updatedProduct) => {
+                    res.redirect(`/sellers/products/${productID}`);
+
+                });
+            }
+        }
+    })
+})
+
+function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2)
+        month = '0' + month;
+    if (day.length < 2)
+        day = '0' + day;
+
+    return [day, month, year].join('-');
+}
 module.exports = router;
